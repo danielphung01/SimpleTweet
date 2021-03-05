b3feabe108397abc839b99e87965ef04ff0fa8d2
danielphung01 <danielphung01@gmail.com>
2021-03-04 18:43:38 -0800

    Required function implemented: - User can **see a counter with total number of characters left for tweet** on compose tweet page
    - walkthrough gif updated

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -3,11 +3,15 @@ package com.codepath.apps.restclienttemplate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
@@ -25,6 +29,7 @@ public class ComposeActivity extends AppCompatActivity {
 
     EditText etCompose;
     Button btnTweet;
+    TextView tvWordCount;
 
     TwitterClient client;
 
@@ -37,6 +42,7 @@ public class ComposeActivity extends AppCompatActivity {
 
         etCompose = findViewById(R.id.etCompose);
         btnTweet = findViewById(R.id.btnTweet);
+        tvWordCount = findViewById(R.id.tvWordCount);
 
         // Set click listener on button
         btnTweet.setOnClickListener(new View.OnClickListener() {
@@ -76,6 +82,35 @@ public class ComposeActivity extends AppCompatActivity {
                         Log.e(TAG, "onFailure to publish tweet", throwable);
                     }
                 });
+            }
+        });
+
+        etCompose.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // Fires right as the text is being changed (even supplies the range of text)
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // Fires right before text is changing
+                tvWordCount.setText(charSequence.length() + "/280");
+                if (charSequence.length() <= 280) {
+                    btnTweet.setClickable(true);
+                    tvWordCount.setTextColor(Color.GRAY);
+                    // Log.i(TAG, "less");
+                } else {
+                    btnTweet.setClickable(false);
+                    tvWordCount.setTextColor(Color.RED);
+                    // Log.i(TAG, "more");
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // Fires right after the text has changed
+
             }
         });
     }
